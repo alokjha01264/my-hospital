d0bfde28d29fed443e3dc79b25f549b4c4c9f8d5
alokjha01264 <alokjha01264@gmail.com>
2025-09-02 17:26:32 +0530

    updated the root response

--- myHospital/urls.py
+++ myHospital/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse  # Import HttpResponse
+
+# Define a simple view function for the homepage
+def home(request):
+    return HttpResponse("<h1>API Server is Running</h1><p>Welcome to the myHospital Backend API.</p>")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),       # For the auth endpoints
     path('api/patients/', include('patients.urls')),   # for patient endpoints
